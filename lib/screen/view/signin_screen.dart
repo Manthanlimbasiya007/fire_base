13b7e7e81c425cec6d2cad790f3cc6ee6be2bf39
rw <jenilchandarana33>
2023-05-26 10:37:20 +0530

    update

--- lib/screen/view/signin_screen.dart
+++ lib/screen/view/signin_screen.dart
@@ -1,49 +1,165 @@
+
+import 'package:data_base/utils/firebase_helper.dart';
 import 'package:flutter/material.dart';
-import '../../utils/firebase_helper.dart';
+import 'package:get/get.dart';
 
 class SignInScreen extends StatefulWidget {
-  const SignInScreen({Key? key}) : super(key: key);
-
   @override
   State<SignInScreen> createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: Text(
-            "SignIn",
-            style: TextStyle(
-              fontSize: 22,
-              color: Colors.white,
-            ),
-          ),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.orange.shade900,
+                  Colors.orange.shade800,
+                  Colors.orange.shade400
+                ]
+            )
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  FirebaseHelper.firebaseHelper.signUp(
-                      email: 'manthan@gmail.com', password: '123456789');
-                },
-                child: Text(
-                  'SignIn',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[Text("SignIn", style: TextStyle(color: Colors.white, fontSize: 40),),
+                  SizedBox(height: 10,),
+                Text("Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),),
+                ],
               ),
             ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 60,),
+                       Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [BoxShadow(
+                                  color: Color.fromRGBO(225, 95, 27, .3),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              )]
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade200),),
+                                ),
+                                child: TextField(
+                                  controller: txtEmail,
+                                  decoration: InputDecoration(
+                                      hintText: "Email or Phone number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey.shade200),),
+                                ),
+                                child: TextField(
+                                  controller: txtPassword,
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 40,),
+                        Text("Forgot Password?", style: TextStyle(color: Colors.grey),),
+                        SizedBox(height: 40,),
+                        InkWell(
+                          onTap: () async{
+                            String email = txtEmail.text;
+                            String password = txtPassword.text;
+                            String msg = await FirebaseHelper.firebaseHelper.signIn(email: "$email", password: "$password");
+                            Get.snackbar("$msg","user added!");
+                            if(msg == "Success")
+                            {
+                              Get.offAndToNamed('/home');
+                            }
+                          },
+                          child: Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 50),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.orange[900]
+                            ),
+                            child: Center(
+                              child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50,),
+                        Text("Continue with social media", style: TextStyle(color: Colors.grey),),
+                        SizedBox(height: 30,),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue
+                                ),
+                                child: Center(
+                                  child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 30,),
+                            Expanded(
+                              child: Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.black
+                                ),
+                                child: Center(
+                                  child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
--- lib/utils/firebase_helper.dart
+++ lib/utils/firebase_helper.dart
@@ -1,15 +1,47 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 
 class FirebaseHelper {
   static FirebaseHelper firebaseHelper = FirebaseHelper._();
+
   FirebaseHelper._();
 
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
-  void signUp({required email, required password}) {
-    firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    // .then((value) => print("Success"))
-    // .catchError(email);
+
+  Future<String> signUp(
+      {required String email, required String password}) async {
+    print("=========== $email $password");
+    String msg = "";
+    await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((value) => msg = "Success")
+        .catchError((e) => msg = " Fail$e");
+    return msg;
+  }
+
+  Future<String> signIn({required email, required password}) async {
+    String? msg;
+    await firebaseAuth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((value) {
+      print("sucessLogin");
+      msg = "Success";
+    }).catchError((e) {
+      print("$e");
+      msg = "$e";
+    });
+    return msg!;
+
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut().then((value) => Get.offAndToNamed('login'));
+  }
+
+  bool checkUser()
+  {
+    User? user = firebaseAuth.currentUser;
+    return user!=null;
   }
 }
